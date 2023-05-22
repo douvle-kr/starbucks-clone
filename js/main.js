ddcf49faa169bc513265ec9d073e1b2b94e7163e
douvle-kr <douvle.kr@kakao.com>
2023-05-22 21:46:47 +0900

    Elements slide

--- js/main.js
+++ js/main.js
@@ -33,3 +33,19 @@ window.addEventListener('scroll', _.throttle(function () {
   }
 }, 300))
 // _.throttle(함수, 시간)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index) {
+  gsap.to(fadeEl, 1,{
+    delay: (index+1)*.7,
+    opacity: 1
+  });
+});
+
+
+new Swiper('.notice-line .swiper', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true,
+});
